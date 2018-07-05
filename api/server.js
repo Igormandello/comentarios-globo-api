d25f123d74947c867cb877a0e23a135e5ff10671
Igormandello <igormandello@gmail.com>
2018-07-04 23:07:12 -0300

    Adicionado servidor
    - Adicionado arquivo server.js na pasta api/
    
    - Servidor rodando em express
    
    - Feito código padrão para a execução do servidor

---
+++ api/server.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.listen(3000, () => console.log('Servidor rodando'));
