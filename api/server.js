52490ff3c116fe5b32fd5408eb526a32d4d646ad
Igormandello <igormandello@gmail.com>
2018-07-05 05:19:27 -0300

    Implementa a busca dos dados necessários para a requisição de comentários
    - A api busca dentro da página da notícia especificada a tag de script que contém as informações que serão usadas na URL da api de comentários
    
    - Adiciona htmlparser2 para o serviço de busca da tag
    
    - Cria referência para o objeto que contém as informações através de um eval() no código original

--- api/server.js
+++ api/server.js
@@ -1,3 +1,4 @@
+const htmlparser = require("htmlparser2");
 const request = require('request');
 const express = require('express');
 const bodyParser = require('body-parser');
@@ -9,26 +10,54 @@ app.use(bodyParser.json());
 app.use('/api/comentarios', async (req, res) => {
   var options = { 
     method: 'GET',
-    url: 'https://comentarios.globo.com/comentarios/%40%40jornalismo%40%40g1%40%40economia/multi-content%40%403f0a93e3-bff8-4071-ad4c-c917d9a93099/https%3A%40%40%40%40g1.globo.com%40%40economia%40%40noticia%40%40producao-industrial-recua-109-em-maio-diz-ibge.ghtml/shorturl/Greve%20dos%20caminhoneiros%20faz%20produ%C3%A7%C3%A3o%20industrial%20recuar%2010%2C9%25%20em%20maio%2C%20diz%20IBGE/1.json',
+    url: 'https://g1.globo.com/economia/noticia/producao-industrial-recua-109-em-maio-diz-ibge.ghtml'
+    //url: 'https://comentarios.globo.com/comentarios/%40%40jornalismo%40%40g1%40%40economia/multi-content%40%403f0a93e3-bff8-4071-ad4c-c917d9a93099/https%3A%40%40%40%40g1.globo.com%40%40economia%40%40noticia%40%40producao-industrial-recua-109-em-maio-diz-ibge.ghtml/shorturl/Greve%20dos%20caminhoneiros%20faz%20produ%C3%A7%C3%A3o%20industrial%20recuar%2010%2C9%25%20em%20maio%2C%20diz%20IBGE/1.json',
   };
 
   let promise = new Promise(resolve => {
-    //A globo fez com que o retorno da api de comentários fosse uma chamada de uma função com esse nome
-    //Era possível fazer apenas um "substring", mas vamos jogar o jogo deles né
-    function __callback_listacomentarios(json) {
-      let textos = [];
-      json.itens.forEach(comentario => {
-        let respostas = [];
-        comentario.replies.forEach(resposta => respostas.push(resposta.texto));
-        textos.push({ mensagem: comentario.texto, respostas: respostas});
-      });
+    request(options, (error, response, body) => {
+      //Pega a tag script com id="SETTINGS", que possui os dados necessários para a requisição em comentarios.globo.com
+      let open = false, settings = '';
+      let parser = new htmlparser.Parser({
+        onopentag: function(name, attribs) {
+          if(name === 'script' && attribs.id == 'SETTINGS')
+            open = true;
+        },
+        ontext: function(text) {
+          if (open)
+            settings += text;
+        },
+        onclosetag: function(tagname) {
+          if(tagname === "script")
+            open = false;
+        }
+      }, { decodeEntities: true });
+      parser.write(body);
+      parser.end();
 
-      resolve(textos);
-    }
+      //Do código nessa string, precisamos apenas do objeto SETTINGS
+      settings = "var " + settings.substring(settings.search("SETTINGS = {"));
+      eval(settings);
 
-    //Como no body vem uma chamada de função, vamos executa-la com eval
-    request(options, function (error, response, body) {
-      eval(body);
+      //Agora possuimos uma referência para o objeto SETTINGS
+      console.log(SETTINGS);
+
+      //Como no body vem uma chamada de função, vamos executa-la com eval
+      options.url = 'https://comentarios.globo.com/comentarios/%40%40jornalismo%40%40g1%40%40economia/multi-content%40%403f0a93e3-bff8-4071-ad4c-c917d9a93099/https%3A%40%40%40%40g1.globo.com%40%40economia%40%40noticia%40%40producao-industrial-recua-109-em-maio-diz-ibge.ghtml/shorturl/Greve%20dos%20caminhoneiros%20faz%20produ%C3%A7%C3%A3o%20industrial%20recuar%2010%2C9%25%20em%20maio%2C%20diz%20IBGE/1.json';
+      request(options, (error, response, body) => eval(body));
+
+      //A globo fez com que o retorno da api de comentários fosse uma chamada de uma função com esse nome
+      //Era possível fazer apenas um "substring", mas vamos jogar o jogo deles né
+      function __callback_listacomentarios(json) {
+        let textos = [];
+        json.itens.forEach(comentario => {
+          let respostas = [];
+          comentario.replies.forEach(resposta => respostas.push(resposta.texto));
+          textos.push({ mensagem: comentario.texto, respostas: respostas});
+        });
+
+        resolve(textos);
+      }
     });
   });
 
